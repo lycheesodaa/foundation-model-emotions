ac83b255c98d14b413022b7886898ae8c72eb0a8
Wei Soon <caedmoncheong@gmail.com>
2024-11-17 17:58:34 +0800

    tf calculation code

--- calc_tf.py
+++ calc_tf.py
@@ -3,8 +3,9 @@ import numpy as np
 import os
 from pathlib import Path
 import matplotlib.pyplot as plt
+from sklearn.metrics import recall_score
 
-directory = Path('results/moirai_pl24')
+directory = Path('results/bilstm')
 
 time_distances = []
 results = {}
@@ -17,7 +18,8 @@ for item in os.listdir(directory):
         f'Files/UF_His_data/step_1_mean/dynamic_next/audio_visual_speaker_{speaker_id}.csv')['time_distance'].tolist()
 
 print(results.keys())
-print('Forecasts shape:', results['1F']['forecasts'].shape)
+# print('Forecasts shape:', results['1F']['forecasts'].shape)
+# print('Forecasts shape:', results['1F']['true'].shape)
 print('Probabilities shape:', results['1F']['probabilities'].shape)
 print('Number of labels:', len(results['1F']['true_labels']))
 
@@ -27,4 +29,31 @@ ax.set_ylabel('time_distances', fontsize=30)
 ax.set_xlabel('utterance_ids', fontsize=30)
 ax.tick_params(axis='both', which='major', labelsize=20)
 ax.set_title('Time distance plots for all utterances in step-1 forecasting', fontsize=25, color='r')
-plt.show()
+# plt.show()
+
+TG_index = {
+    1: [index for index, time in enumerate(time_distances) if 0 < time < 6],
+    2: [index for index, time in enumerate(time_distances) if 6 < time < 12],
+    3: [index for index, time in enumerate(time_distances) if 12 < time < 18]
+}
+
+all_gt = []
+all_pred = []
+for speaker in results.keys():
+    all_gt.append(results[speaker]['true_labels'])
+    all_pred.append(np.argmax(results[speaker]['probabilities'], axis=1))
+
+all_gt = np.concatenate(all_gt)
+all_pred = np.concatenate(all_pred)
+
+print('GT shape:', all_gt.shape)
+print('Predictions shape:', all_pred.shape)
+print('Time distances length:', len(time_distances))
+
+GT_timewise= {}
+Pred_timewise = {}
+for time_group in TG_index.keys():
+    GT_timewise[time_group] = [all_gt[i] for i in TG_index[time_group]]
+    Pred_timewise[time_group] = [all_pred[i] for i in TG_index[time_group]]
+    RECALL =  recall_score(GT_timewise[time_group],  Pred_timewise[time_group], average='macro')
+    print('Performance for group {0} is {1:.2f}%'.format(time_group, RECALL*100))
